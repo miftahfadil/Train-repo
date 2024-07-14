6d483f90f7a64034db870661eca48baff9767b1a
Miftah Fadil <miftahfadilv@gmail.com>
2024-07-14 16:42:38 +0700

    Mengubah fungsi greeting_name

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 print('hello! this is my code.')
 
-def greeting_name(name:str='fadil')->None:
-    print(f'halo, {name.upper()}! Senang bertemu denganmu.')
+def greeting_name(name:str='fadil')->str:
+    return f'halo, {name.upper()}! Senang bertemu denganmu.'
 
 if __name__ == '__main__':
-    greeting_name('miftah')
-    greeting_name()
+    name1 = greeting_name('miftah')
+    name2 = greeting_name()
+
+    print(name1)
+    print(name2)
