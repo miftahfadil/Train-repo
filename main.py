7743d1ce37f330fbf539cb825c0c5726b2035c68
Miftah Fadil <miftahfadilv@gmail.com>
2024-07-14 15:55:02 +0700

    Mengubah fungsi greeting_name dengan menambahkan method .upper()

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 print('hello! this is my code.')
 
 def greeting_name(name:str='fadil')->None:
-    print(f'halo, {name}! Senang bertemu denganmu.')
+    print(f'halo, {name.upper()}! Senang bertemu denganmu.')
 
 if __name__ == '__main__':
-    greeting_name()    
+    greeting_name('miftah')
+    greeting_name()
