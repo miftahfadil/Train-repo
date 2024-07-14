013851f2ecbfba0b3fe6f0a7dcdb1c98cd862011
Miftah Fadil <miftahfadilv@gmail.com>
2024-07-14 16:46:50 +0700

    Mengubah file main.py

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ def greeting_name(name:str='fadil')->None:
     print(f'halo, {name.upper()}! Senang bertemu denganmu.')
 
 if __name__ == '__main__':
-    greeting_name('miftah')
+    names = ['miftah', 'sopian']
+    for name in names:
+        greeting_name(name)
     greeting_name()
